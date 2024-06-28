a484c462e9ca1ce410de4eefe8887e1c3313318c
sumansingh05 <kumarsumansingh7870@gmail.com>
2024-06-28 22:19:25 +0530

    modified code

--- assignment3.py
+++ assignment3.py
@@ -8,3 +8,50 @@ class BinaryTree:
     def __init__(self):
         self.root = None
         self.count = -1
+
+    def buildTree(self,nodes):
+        self.count += 1
+        if nodes[self.count] == -1:
+            return None
+        newNode = Node(nodes[self.count])
+        newNode.left = self.buildTree(nodes)
+        newNode.right = self.buildTree(nodes)
+          
+        return newNode
+    
+    def preorder(self,root):
+        if root is None:
+            return
+        print(root.data,end=" ")
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+    def inorder(self,root):
+        if root is None:
+            return        
+        self.inorder(root.left)
+        print(root.data,end=" ")
+        self.inorder(root.right)
+
+    def postorder(self,root):
+        if root is None:
+            return        
+        self.postorder(root.left)        
+        self.postorder(root.right)
+        print(root.data,end=" ")
+
+nodes = [1,12,14,-1,-1,15,-1,-1,13,-1,16,-1,-1]
+tree = BinaryTree()
+root = tree.buildTree(nodes)
+print("Root of Tree is ",root.data)
+
+print("Preorder Traversal:", end=" ")
+tree.preorder(root)
+print()
+print("Inorder Traversal:", end=" ")
+tree.inorder(root)
+print()
+
+print("Postorder Traversal:", end=" ")
+tree.postorder(root)
+print()
